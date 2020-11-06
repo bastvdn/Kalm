f60f8392fdd43fc97380fe0ece8b9ad4dcc2f718
unknown <bastvdn@gmail.com>
2020-11-06 21:15:02 +0100

    v2.0

--- kalm.py
+++ kalm.py
@@ -40,6 +40,7 @@ if __name__ == "__main__":
             if now.hour < 8 or now.hour > 18:
                 data = np.frombuffer(stream.read(CHUNK),dtype=np.int16)
                 peak=np.average(np.abs(data))*2
+                print(peak)
                 #print("%04d %05d"%(i,peak))
                 if peak > LOWTHRESHOLD and peak <= HIGHTHRESHOLD + 10:
                     if flag != 0:
@@ -52,15 +53,19 @@ if __name__ == "__main__":
                     # 2015 5 6 8 53 40
                     if flag > 4:
                         #   toaster.show_toast("%dh%s" % (now.hour,now.minute), "Ferme ta ras HMAR",duration = 5)
+                        now = datetime.datetime.now()
                         notification.notify("%dh%s" % (now.hour,now.minute), "Ferme ta ras HMAR",timeout=5)
                         time.sleep(6)
                         stream.start_stream()
                         flag = 0
                 elif peak > HIGHTHRESHOLD:
-
+                    stream.stop_stream()
+                    now = datetime.datetime.now()
+                    peak = 0
                     notification.notify("%dh%s" % (now.hour, now.minute), "Ferme ta ras HMAR", timeout=5)
                     time.sleep(6)
                     stream.start_stream()
+                    data = np.frombuffer(stream.read(CHUNK), dtype=np.int16)
                     flag = 0
                 else:
                     flag = 0
